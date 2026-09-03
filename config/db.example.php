c4d9e35476932582ccebe04ad91be7089263f5c8
AlbertAvagyann <albert.a@humaindevsystems.com>
2026-09-03 12:31:24 +0400

    Added .gitignore and db example

---
+++ config/db.example.php
@@ -0,0 +1,17 @@
+<?php
+$host   = 'localhost';
+$dbname = 'basicauthorization_app';
+$dbuser = 'root';
+$dbpass = 'your_password_here';
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $dbuser,
+        $dbpass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Database connected successfully!";
+} catch (PDOException $e) {
+    die('Database connection failed: ' . $e->getMessage());
+}
